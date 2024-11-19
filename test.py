7efdf0742e81206d86f4300d5ddf6a284b19b6a0
Luna <git@l4.pm>
2024-11-19 01:32:29 -0300

    make test.py run continuously

--- test.py
+++ test.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 import requests
+import time
 
 resp_initial = requests.get("http://localhost:4000/api/v1/hi")
 assert resp_initial.status_code == 200
 rjson = resp_initial.json()
 posts = rjson["batch"]
-print(len(posts), "posts")
-post = posts[-200]
-post_timestamp = int(post["d"]) % 1000
-print("req post timestamp", post_timestamp)
-resp_delta = requests.get(f"http://localhost:4000/api/v1/s/{post_timestamp}")
-djson = resp_delta.json()
-delta_posts = djson["batch"]
-assert len(delta_posts) < len(posts)
-print(len(posts), "initial sync posts")
-print(len(delta_posts), "delta posts")
+first = True
+while True:
+    print("have", len(posts))
+    post = posts[-1]
+    post_timestamp = int(post["d"]) % 1000
+    print("requesting at timestamp", post_timestamp)
+    resp_delta = requests.get(f"http://localhost:4000/api/v1/s/{post_timestamp}")
+    djson = resp_delta.json()
+    delta_posts = djson["batch"]
+    if first:
+        assert len(delta_posts) <= len(posts)
+        first = False
+    print("got", len(delta_posts), "delta posts, sleeping...")
+    posts = delta_posts
+    time.sleep(5)
