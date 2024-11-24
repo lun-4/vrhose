ec08cec5c6c70de5921499d93ef5dcb1bcbd6d48
Luna <git@l4.pm>
2024-11-24 00:54:55 -0300

    fix random build bug

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule VRHose.MixProject do
   def application do
     [
       mod: {VRHose.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :inets]
     ]
   end
 
