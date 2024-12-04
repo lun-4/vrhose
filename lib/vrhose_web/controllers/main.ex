aee709475877eb63923881034cf59fee03b6d87c
Luna <git@l4.pm>
2024-12-03 23:46:26 -0300

    be more resilient on worldspace rollover

--- lib/vrhose_web/controllers/main.ex
+++ lib/vrhose_web/controllers/main.ex
@@ -14,6 +14,7 @@ defmodule VRHoseWeb.MainController do
   end
 
   def fetch_delta(conn, %{"timestamp" => worldspace_timestamp_str}) do
+    current_server_timestamp = DateTime.utc_now() |> DateTime.to_unix()
     {worldspace_timestamp, ""} = Integer.parse(worldspace_timestamp_str, 10)
 
     # we need to convert from worldspace to serverspace
@@ -23,11 +24,17 @@ defmodule VRHoseWeb.MainController do
     # - only 1000 seconds resolution (thats fine for us)
 
     # basically utc now but ending in 000 lol
-    base_timestamp = (((DateTime.utc_now() |> DateTime.to_unix()) / 1000) |> trunc) * 1000
+    base_timestamp = ((current_server_timestamp / 1000) |> trunc) * 1000
 
-    # add worldspace to get the second
+    server_delta_in_worldspace = current_server_timestamp - base_timestamp
+
     realspace_timestamp =
-      base_timestamp + worldspace_timestamp
+      if server_delta_in_worldspace < 100 and worldspace_timestamp > 900 do
+        # use base_timestamp from previous cycle
+        base_timestamp - 1000 + worldspace_timestamp
+      else
+        base_timestamp + worldspace_timestamp
+      end
 
     {:ok, timeline} =
       Registry.lookup(Registry.Timeliners, "timeliner")
